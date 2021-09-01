6006b6d4d886e70cc850fa5baac33e7e10e6506f
EMI <emiliano.2.farias@hotmail.com>
2021-08-31 23:02:07 -0300

    Version n1 del carrusel, aun hay unos detalles

--- src/components/article.js
+++ src/components/article.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { createRef } from 'react';
 
 class Article extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.news = createRef();
+  }
   render() {
+    // this.handleClick = () => {
+    //   console.log(news.current);
+    // };
     return (
-        <article className='new'>
+      <article className="new" onClick={this.handleClick}>
         <div className={this.props.NroDeTitulo}>
-          <a className="linkNews" href={this.props.Redireccion}><h1>{this.props.Titulo}</h1></a>
-         </div>
-          
-          <figure className='newImageContainer'>
-            <img src={this.props.ImageURL}alt="ARTICULO" className="newImagen"/>
-            <div className="layerNew">
-              <h2>{this.props.SubtituloInterno}</h2>
-              <p>{this.props.Descripcion}</p>
-            </div>
-          </figure>
-       </article>
+          <a className="linkNews" href={this.props.Redireccion}>
+            <h1>{this.props.Titulo}</h1>
+          </a>
+        </div>
+
+        <figure className="newImageContainer">
+          <img src={this.props.ImageURL} alt="ARTICULO" className="newImagen" />
+          <div className="layerNew">
+            <h2>{this.props.SubtituloInterno}</h2>
+            <p>{this.props.Descripcion}</p>
+          </div>
+        </figure>
+      </article>
     );
   }
 }
