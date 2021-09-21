7d7be5db7e0b992cca38cae1977d8af67bd02cd9
EMI <emiliano.2.farias@hotmail.com>
2021-09-21 18:45:58 -0300

    Prueba de codigo para las flechas y modificación en algunos estilos en el carrusel

--- src/components/news.js
+++ src/components/news.js
@@ -1,5 +1,5 @@
 import React, { createRef } from 'react';
-import arrow from '../fonts/arrow.svg';
+// import arrow from '../fonts/arrow.svg';
 
 import Article from './article';
 
@@ -20,11 +20,8 @@ class News extends React.Component {
       let widthContainerNew = containerNews.current.clientWidth;
       let maxScroll =
         containerNews.current.scrollWidth - widthContainerNew - 210;
-      // console.log(maxScroll);
-      // let moveTo = 898;
       if (moveTo < maxScroll) {
         moveTo += widthContainerNew;
-        // console.log(moveTo);
         containerNews.current.scrollTo(moveTo, 0);
       }
       return moveTo;
@@ -47,14 +44,38 @@ class News extends React.Component {
             onClick={this.handleClickPrevius}
             className="carousel__anterior"
           >
-            <img src={arrow} />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              viewBox="0 0 13.503 23.619"
+            >
+              <path
+                id="Icon_ionic-ios-arrow-back"
+                data-name="Icon ionic-ios-arrow-back"
+                d="M15.321,18l8.937-8.93a1.688,1.688,0,0,0-2.391-2.384L11.742,16.8a1.685,1.685,0,0,0-.049,2.327L21.86,29.32a1.688,1.688,0,0,0,2.391-2.384Z"
+                transform="translate(-11.251 -6.194)"
+                fill="#fff"
+              />
+            </svg>
           </button>
           <button
             ref={afterBtn}
             onClick={this.handleClickAfter}
             className="carousel__siguiente"
           >
-            <img src={arrow} />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              viewBox="0 0 13.503 23.619"
+            >
+              <path
+                id="Icon_ionic-ios-arrow-back"
+                data-name="Icon ionic-ios-arrow-back"
+                d="M15.321,18l8.937-8.93a1.688,1.688,0,0,0-2.391-2.384L11.742,16.8a1.685,1.685,0,0,0-.049,2.327L21.86,29.32a1.688,1.688,0,0,0,2.391-2.384Z"
+                transform="translate(-11.251 -6.194)"
+                fill="#fff"
+              />
+            </svg>
           </button>
           <div className="titleNews">
             <h1>ULTIMAS NOVEDADES</h1>
